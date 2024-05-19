360f1f1e7b7de6a002c8af458d19651278a30861
tomoakiWeb <tomomiura0026@gmail.com>
2024-05-19 23:51:24 +0900

    MainActivityの作成

--- composeApp/src/androidMain/kotlin/com/qittaList/ArticleViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/qittaList/ArticleViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.qittaList.QittaSDK
 import kotlinx.coroutines.launch
 
 class ArticleViewModel(private val sdk: QittaSDK) : ViewModel() {
