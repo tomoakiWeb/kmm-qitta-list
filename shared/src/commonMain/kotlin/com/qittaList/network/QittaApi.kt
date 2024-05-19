3809972872336f02d37f2368c4933b09ebc1efb2
tomoakiWeb <tomomiura0026@gmail.com>
2024-05-19 23:05:37 +0900

    QittaApiの作成

---
+++ shared/src/commonMain/kotlin/com/qittaList/network/QittaApi.kt
@@ -0,0 +1,23 @@
+package com.qittaList.network
+
+import Article
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class QittaApi {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                useAlternativeNames = false
+            })
+        }
+    }
+    suspend fun getAllLaunches(): List<Article> {
+        return httpClient.get("https://qiita.com/api/v2/items").body()
+    }
+}
