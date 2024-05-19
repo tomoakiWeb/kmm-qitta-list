f5a0061d9bafce10549c6f687a1fe520c273f9b7
tomoakiWeb <tomomiura0026@gmail.com>
2024-05-19 23:05:09 +0900

    Entitlyの修正

---
+++ shared/src/commonMain/kotlin/com/qittaList/Entity.kt
@@ -0,0 +1,19 @@
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id: String,
+    val name: String,
+    @SerialName("profile_image_url") val profileImageUrl: String
+)
+
+@Serializable
+data class Article(
+    val id: String,
+    val title: String,
+    val body: String,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("updated_at") val updatedAt: String,
+    val user: User
+)
