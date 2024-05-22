273383cd3f034ca80b5ecde44f71fe4327e6c83b
tomoakiWeb <tomomiura0026@gmail.com>
2024-05-22 23:35:42 +0900

    ArticleRowのレイアウト修正

--- iosApp/iosApp/ArticleRow.swift
+++ iosApp/iosApp/ArticleRow.swift
@@ -5,6 +5,24 @@ struct ArticleRow: View {
     var article: Article
 
     var body: some View {
-        Text(article.title)
+        
+        VStack(spacing: 8.0) {
+            Text(article.title)
+                .font(.title2)
+            
+            if !article.user.name.isEmpty {
+                HStack(alignment: .center, spacing: 8.0) {
+                    if let profileImageUrl = URL(string: article.user.profileImageUrl) {
+                        AsyncImage(url: profileImageUrl)
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                        
+                        Text(article.user.name)
+                    }
+                    Spacer()
+                }
+            }
+        }
     }
 }
